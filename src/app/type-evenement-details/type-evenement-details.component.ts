93ab31212e2cf493b4826a4d5cd5b9037a87a898
Adnen-Developer <adnen.benabdelaali@esprit.tn>
2022-07-20 18:38:24 +0200

    ddsfdd

--- src/app/type-evenement-details/type-evenement-details.component.ts
+++ src/app/type-evenement-details/type-evenement-details.component.ts
@@ -1,4 +1,11 @@
+
+
 import { Component, OnInit } from '@angular/core';
+import {TypeEvenement} from "../model/typeEvenement";
+import {TypeEvenementService} from "../services/type-evenement.service";
+
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-type-evenement-details',
@@ -6,10 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type-evenement-details.component.css']
 })
 export class TypeEvenementDetailsComponent implements OnInit {
+  
+  typeEvenement: TypeEvenement;
 
-  constructor() { }
+  _id: string;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private typeEvenementService:TypeEvenementService) { }
+
+  
+    
+
+    ngOnInit() : void{
+
+      this.typeEvenement= new TypeEvenement();
+      
+      this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // { orderby: "price" }
+        this._id = params._id;
+        console.log(this._id); // price
+      }
+    );
+    
+
+
+  }
+  save(){
+    //
+ //   this.book.nbrLike=0;
+    this.typeEvenementService.addTypeEvenement(this.typeEvenement).subscribe();
+    //notify
+
   }
 
 }
+
+
