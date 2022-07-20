ac690b91597fc2aa4bf71d51fae37dea78867f3a
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2022-07-20 23:27:05 +0200

    change

--- src/app/evenement-form/evenement-form.component.ts
+++ src/app/evenement-form/evenement-form.component.ts
@@ -1,4 +1,9 @@
+
+
+
 import { Component, OnInit } from '@angular/core';
+import {Evenement} from "../model/evenement";
+import {EvenementService} from "../services/evenement.service";
 
 @Component({
   selector: 'app-evenement-form',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evenement-form.component.css']
 })
 export class EvenementFormComponent implements OnInit {
+  evenement: Evenement;
 
-  constructor() { }
+  constructor(private evenementService:EvenementService) { }
 
   ngOnInit(): void {
+    this.evenement= new Evenement();
+  }
+  save(){
+    //
+ //   this.book.nbrLike=0;
+    this.evenementService.addEvenement(this.evenement).subscribe();
+    //notify
+
   }
 
 }
+
+
